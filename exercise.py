b72cdc60abac94a740518618c8f471f764cd3a41
胡率龙 <921989101@qq.com>
2022-10-09 22:06:04 +0800

    提交

--- exercise.py
+++ exercise.py
@@ -7,22 +7,102 @@ def example():
     r = 3*np.ones(100)
     plt.plot(theta,r,color='b')
     plt.show()
+def subplot():
+    #plot 1:
+    xpoints = np.array([0, 6])
+    ypoints = np.array([0, 100])
 
-#plot 1:
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 100])
+    plt.subplot(1, 2, 1)#一行两列第一图，初始化一个Axes，并默认一个figure？大概
+    plt.plot(xpoints,ypoints)#在上Axes绘图
+    plt.title("plot 1")
 
-plt.subplot(1, 2, 1)
-plt.plot(xpoints,ypoints)
-plt.title("plot 1")
+    #plot 2:
+    x = np.array([1, 2, 3, 4])
+    y = np.array([1, 4, 9, 16])
 
-#plot 2:
-x = np.array([1, 2, 3, 4])
-y = np.array([1, 4, 9, 16])
+    plt.subplot(1, 2, 2)#一行两列第二图，初始化一个Axes？大概
+    plt.plot(x,y)#在上Axes绘图
+    plt.title("plot 2")
 
-plt.subplot(1, 2, 2)
-plt.plot(x,y)
-plt.title("plot 2")
+    plt.suptitle("RUNOOB subplot Test")#figure标题，大概
+    plt.show()
 
-plt.suptitle("RUNOOB subplot Test")
-plt.show()
+def subplots():
+    # 创建一些测试数据
+    x = np.linspace(0, 2*np.pi, 400)
+    y = np.sin(x**2)
+
+    # 创建一个画像和子图 -- 图1
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    ax.set_title('Simple plot')
+
+    # 创建两个子图 -- 图2
+    f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+    ax1.plot(x, y)
+    ax1.set_title('Sharing Y axis')
+    ax2.scatter(x, y)
+
+    # # 创建四个子图 -- 图4
+    # fig, axs = plt.subplots(2, 2, subplot_kw=dict(projection="polar"))
+    # axs[0, 0].plot(x, y)
+    # axs[1, 1].scatter(x, y)
+    #
+    # # 共享 x 轴
+    # plt.subplots(2, 2, sharex='col')
+    #
+    # # 共享 y 轴
+    # plt.subplots(2, 2, sharey='row')
+    #
+    # # 共享 x 轴和 y 轴
+    # plt.subplots(2, 2, sharex='all', sharey='all')
+    #
+    # # 这个也是共享 x 轴和 y 轴
+    # plt.subplots(2, 2, sharex=True, sharey=True)
+    #
+    # # 创建标识为 10 的图，已经存在的则删除
+    # fig, ax = plt.subplots(num=10, clear=True)
+
+    plt.show()
+
+def example_坐标系对象数组():
+    theta = np.linspace(0,2*math.pi,100)
+    r = np.ones(100)
+
+    plt.figure()#创建画布
+    axs= {}#创建坐标系对象的数组，使用[]报错
+
+    for i in range(9):#以下行为皆在上行打开的figure中展开，大概
+
+        axs[i]=plt.subplot(111,projection='polar')#将创造的极坐标系存为对象
+
+        axs[i].scatter(1*i,1*i,color='k')#使用对象名与方法操作对象
+        axs[i].scatter(5*i,5*i,color='k')
+        axs[i].scatter(10*i,10*i,color='k')
+        axs[i].plot(theta,2*r,color='b')
+
+    plt.show()#展示figure
+
+def hh():
+    theta = np.linspace(0, 2 * math.pi, 100)
+    r = np.ones(100)
+    plt.ion()#打开交互模式
+    plt.figure()  # 创建画布
+    axs = {}  # 创建坐标系对象的数组，使用[]报错
+
+    for i in range(9):  # 以下行为皆在上行打开的figure中展开，大概
+        #plt.clf()#清理figure
+        plt.cla()#清理Axes
+        #由于每次都是在新的Axes上绘制，显示，故cla做一个清一个，clf做一个清全部小题大做
+        axs[i] = plt.subplot(111, projection='polar')  # 将创造的极坐标系存为对象
+
+
+        axs[i].scatter(1 * i, 1 * i, color='k')  # 使用对象名与方法操作对象
+        axs[i].scatter(5 * i, 5 * i, color='k')
+        axs[i].scatter(10 * i, 10 * i, color='k')
+        axs[i].plot(theta, 2 * r, color='b')
+
+        plt.pause(1)#暂停一秒
+    plt.ioff()#关闭交互模式
+
+hh()
--- main.py
+++ main.py
@@ -61,13 +61,13 @@ class heavenlyBodies():
     def __init__(self,size,r,o,time,color):
         self.size = size*20
         self.xy = [int(r*5),o]
-        self.speed = (2*math.pi)/time
+        self.speed = ((2*math.pi)/time)*2
         self.color = color
 
     def move(self):
         self.xy[1]+= self.speed
-        while self.xy[1]>=2*math.pi:
-            self.xy[1]-=2*math.pi
+        # while self.xy[1]>=2*math.pi:#极坐标可以适应大于2pi的数，无需处理
+        #     self.xy[1]-=2*math.pi
 
     def draw(self):
         plt.scatter(self.xy[1],self.xy[0],self.size,self.color)
@@ -76,27 +76,42 @@ def body_data():
     global orbit_radius
     global orbit_time
     global body_color
+    global orbit_theta
+    global star_x
+    global star_r
+    global star_l
     #太阳系天体半径适当化：太阳20水星1金星3地球3火星2木星7土星6天王星5海王星5
-    body_radius = [20,1,3,3,2,7,6,5,5]
+    body_radius = [50,1,2.4,2.6,1.4,23,19,7,7]
     #太阳系天体轨道半径适当化：太阳0水星4金星8地球10火星15木星20土星30天王星40海王星50
-    orbit_radius = [0,4,8,10,15,20,30,40,50]
+    orbit_radius = [0,20,26,32,44,64,94,132,174]
     #太阳系天体公转周期适当化：太阳0水星80金星200地球400火星800木星4000土星10000天王星30000海王星60000
     orbit_time = [1,80,200,400,800,4000,10000,30000,60000]
+    #太阳系天体初始角度适当化
+    orbit_theta = 2*math.pi*np.random.rand(9)
     #太阳系天体颜色适当化：太阳 深橙 水星 金黄 金星 金黄 地球 深蓝 火星 黄土赭 木星 秋麒麟 土星 耐火砖 天王星 浅蓝 海王星 蓝
     body_color = ['#FF8C00','#FFD700','#FFD700','#0000FF','#A0522D','#DAA520','#B22222','#00FFFF','#4682B4',]
+    #星空背景数据
+    star_x = 2000*np.random.rand(1000)
+    star_r = np.random.rand(1000)
+    star_l = np.random.rand(1000)
 body_data()#导入数据
 
 sun_body = []
 for i in range(9):#创建天体对象
-    sun_body.append(heavenlyBodies(body_radius[i],orbit_radius[i],0,orbit_time[i],body_color[i]))
+    sun_body.append(heavenlyBodies(body_radius[i],orbit_radius[i],orbit_theta[i],orbit_time[i],body_color[i]))
 
 
-plt.figure(1, (10, 10))  # 打开画布
+
 plt.ion()
 while 1:
+    plt.figure(1, (10, 10), facecolor='k')  # 打开画布
     plt.clf()
-    plt.subplot(projection='polar')#切换坐标系为极坐标
+
+    plt.subplot(projection='polar',facecolor='k')#切换坐标系为极坐标
+    plt.xlim(0, 2*math.pi)#x在极坐标下控制角度
+    plt.ylim(0, 900)#y在极坐标系下控制半径
     plt.grid(False)#不显示格子
+    plt.scatter(star_x,(star_x+200),s=star_r,c='white',alpha=star_l)#显示星星
     therta = np.linspace(0,2*math.pi)#轨道角度数据
     for i in range(9):#绘制轨道
         r = sun_body[i].xy[0]*np.ones(50)#轨道半径数据
